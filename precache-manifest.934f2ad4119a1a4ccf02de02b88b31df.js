b69cb5acb08804d762041c48a391da3b2ea3c725
supfiger <danilzin777@gmail.com>
2020-05-15 18:17:09 +0300

    Updates

---
+++ precache-manifest.934f2ad4119a1a4ccf02de02b88b31df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2b8971d0587c48fce55a6b7faf2b05b",
+    "url": "/starnavi-game-in-dots/index.html"
+  },
+  {
+    "revision": "7886d9ad51efb2f982b5",
+    "url": "/starnavi-game-in-dots/static/css/main.9cbdee19.chunk.css"
+  },
+  {
+    "revision": "f6f70962b0f963072979",
+    "url": "/starnavi-game-in-dots/static/js/2.36e1eb18.chunk.js"
+  },
+  {
+    "revision": "c836aaadfe43dfd9fcb8ddeddfd61e18",
+    "url": "/starnavi-game-in-dots/static/js/2.36e1eb18.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7886d9ad51efb2f982b5",
+    "url": "/starnavi-game-in-dots/static/js/main.f9f45ac9.chunk.js"
+  },
+  {
+    "revision": "89549aa616d220470886",
+    "url": "/starnavi-game-in-dots/static/js/runtime-main.57e8eaa7.js"
+  }
+]);
--- src/components/index.js
+++
@@ -1,6 +0,0 @@
-export { default as Board } from "./Board/Board";
-export { default as Dot } from "./Dot/Dot";
-export { default as Field } from "./Field/Field";
-export { default as Game } from "./Game/Game";
-export { default as Message } from "./Message/Message";
-export { default as Panel } from "./Panel/Panel";
